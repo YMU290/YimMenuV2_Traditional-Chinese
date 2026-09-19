a5d19591fa31ad1fae01465be4ec61eb778465a5
Tommy <tommylam120@gmail.com>
2026-09-19 18:55:47 +0800

    Del

--- src/game/frontend/submenus/Vehicle/VehicleEditor.hpp
+++
@@ -1,7 +0,0 @@
-#include "core/frontend/manager/Category.hpp"
-#include "game/frontend/items/Items.hpp"
-
-namespace YimMenu::Submenus
-{
-	std::shared_ptr<Category> BuildVehicleEditorMenu();
-}
--- src/game/scripting/libraries/NativeDefs.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-extern const char* g_LuaNativeDefs[];
-extern int g_NumLuaNativeDefs;
