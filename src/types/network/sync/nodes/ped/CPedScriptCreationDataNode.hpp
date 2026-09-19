c910e5a1e89f51b45d28b5f071bd21d318df13c6
Tommy <tommylam120@gmail.com>
2026-09-19 21:07:07 +0800

    Del

--- src/types/network/sync/nodes/ped/CPedScriptCreationDataNode.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "types/network/sync/CProjectBaseSyncDataNode.hpp"
-
-class CPedScriptCreationDataNode : CProjectBaseSyncDataNode
-{
-public:
-	bool m_StayInCarWhenJacked; //0x00C0
-}; //Size: 0x00C1
-static_assert(sizeof(CPedScriptCreationDataNode) == 0xC8);
